eb6c1e161d9711ba6242f697eba9c18c19d21daf
Mutah3r <evil.programmer.stephen@gmail.com>
2025-10-21 14:40:52 +0600

    add animation to faq title and remove the search bar from the faq section

--- front-end/src/components/modules/home/FAQ.tsx
+++ front-end/src/components/modules/home/FAQ.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { ChevronDown, ChevronUp } from "lucide-react";
+import { TextFade } from "@/components/shared/TextFade";
 
 export default function Faq() {
   const [openIndex, setOpenIndex] = useState(null);
@@ -35,21 +36,23 @@ export default function Faq() {
 
   return (
     <div className="w-10/12 mt-28 mx-auto   space-y-4">
-      <h1 className="font-bold text-center lg:text-[36px] text-xl">
-        Frequently <br /> Asked Questions
-      </h1>
-      <div className="w-full max-w-lg  mx-auto mt-6 mb-10">
+      <TextFade direction="up" staggerChildren={0.15}>
+        <h1 className="font-bold text-center lg:text-[36px] text-xl">
+          Frequently <br /> Asked Questions
+        </h1>
+      </TextFade>
+      {/* <div className="w-full max-w-lg  mx-auto mt-6 mb-10">
         <input
           type="text"
           placeholder="Search your questions"
           className="w-full px-4 py-2 rounded-full  bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500 text-gray-900 placeholder-gray-400"
         />
-      </div>
-      <div className="bg-gradient-to-r from-[#f9fbff] via-[#e3f3ff] to-[#f9fbff] space-y-5">
+      </div> */}
+      <div className="bg-gradient-to-r mt-10 from-[#f9fbff] via-[#e3f3ff] to-[#f9fbff] space-y-5">
         {faqs.map((faq, index) => (
           <div
             key={index}
-            className="bg-white rounded-xl shadow p-5 cursor-pointer transition"
+            className="bg-white rounded-xl p-5 cursor-pointer transition"
             onClick={() => toggleFAQ(index)}
           >
             <div className="flex justify-between  items-center">
